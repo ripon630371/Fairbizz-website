0e6e91f99773e30d731d13ac4e7de0d6828d5430
ripon535899 <riponcmt630371@gmail.com>
2019-06-27 16:20:00 +0600

    menu and slider ok

---
+++ js/main.js
@@ -0,0 +1,52 @@
+(function ($) {
+"use strict";
+
+
+// slick mainSlider
+function mainSlider() {
+	var BasicSlider = $('.slider-active');
+	BasicSlider.on('init', function (e, slick) {
+		var $firstAnimatingElements = $('.single-slider:first-child').find('[data-animation]');
+		doAnimations($firstAnimatingElements);
+	});
+	BasicSlider.on('beforeChange', function (e, slick, currentSlide, nextSlide) {
+		var $animatingElements = $('.single-slider[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
+		doAnimations($animatingElements);
+	});
+	BasicSlider.slick({
+		autoplaySpeed: 2000,
+		dots: false,
+		fade: true,
+		arrows: true,
+		slidesToShow: 1,
+      	slidesToScroll: 1,
+      	autoplay: true,
+      	autoplaySpeed: 2000,
+
+		prevArrow:'<button type="button" class="slick-prev"><i class="lnr lnr-arrow-left"></i>prev</button>',
+		nextArrow:'<button type="button" class="slick-next"><i class="lnr lnr-arrow-right"></i>next</button>',
+		responsive: [
+			{ breakpoint: 767, settings: { dots: false, arrows: false } }
+		]
+	});
+
+	function doAnimations(elements) {
+		var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+		elements.each(function () {
+			var $this = $(this);
+			var $animationDelay = $this.data('delay');
+			var $animationType = 'animated ' + $this.data('animation');
+			$this.css({
+				'animation-delay': $animationDelay,
+				'-webkit-animation-delay': $animationDelay
+			});
+			$this.addClass($animationType).one(animationEndEvents, function () {
+				$this.removeClass($animationType);
+			});
+		});
+	}
+}
+mainSlider();
+
+
+})(jQuery);	
