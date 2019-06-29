652fc3f4ce4b2c9fb15dc00acd358ac634932867
ripon535899 <riponcmt630371@gmail.com>
2019-06-29 12:40:14 +0600

    service slider ok

--- js/main.js
+++ js/main.js
@@ -48,5 +48,44 @@ function mainSlider() {
 }
 mainSlider();
 
+/*service slider here*/
+$('.service-active').slick({
+  dots: true,
+  arrows: false,
+  infinite: false,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+
 
 })(jQuery);	
